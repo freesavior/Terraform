024c7dd4a17f104d6928aa6d4dc267f22ffb0084
freesavior <92399234+freesavior@users.noreply.github.com>
2023-07-09 06:31:47 +0200

    Add files via upload

---
+++ modules/auto_scaling/variables.tf
@@ -0,0 +1,18 @@
+variable"image_id"{
+    description="ID de l'image AMI souhaitée"
+    type=string
+    default= "ami-05432c5a0f7b1bfd0"
+    }
+
+variable"instance_type"{
+    description="Type d'instance souhaité"
+    type=string
+    default="t2.micro"
+    }
+
+# variable"key_name"{
+#     description="Nom de la paire de clés SSH"
+#     type=string
+#     default="Devops.pem"
+#     }
+    
